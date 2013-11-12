f5427f17333260f912e1edc32c4b7deac15cee19
taoyuan <taoyuan@oyo.im>
2013-11-12 11:08:18 +0800

    refactor

--- release/angular-widgets.debug.js
+++ release/angular-widgets.debug.js
@@ -2,7 +2,7 @@
 * angular-widgets JavaScript Library
 * Authors: https://github.com/torworx/angular-widgets/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 2013-11-11 16:59
+* Compiled At: 2013-11-12 10:51
 ***********************************************/
 (function(window, $) {
 'use strict';
